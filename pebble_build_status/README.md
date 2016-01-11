36c00ee393e4714f9f7518c06408e5d5dede3a32
Kevin Conley <kevinc@getpebble.com>
2016-01-11 00:28:34 -0800

    Make Pebble build status daemon script PEP-8 compliant

---
+++ pebble_build_status/README.md
@@ -0,0 +1,16 @@
+# Pebble Build Status
+
+Use the blink(1) to communicate the state of Pebble's master build status.
+Green is OK, Red is :(, Orange is request failed.
+
+## Setup
+
+Install pip requirements:
+
+```
+pip install -r requirements.txt
+```
+
+Make sure you're connected to the Pebble network either locally or via VPN.
+
+Lastly, follow the steps to setup the daemon in the README in the root of this repository.
--- pebble_build_status/pebble_build_status.py
+++ pebble_build_status/pebble_build_status.py
@@ -1,10 +1,11 @@
-# Use the blink(1) to communicate the state of our continuous integration system
+# Use the blink(1) to communicate the state of Pebble's master build status
 # Green is OK, Red is :(, Orange is request failed
 
 import atexit
 import os
 import requests
 import sh
+import sys
 from time import sleep
 
 WALTER_MASTER_STATUS_URL = 'http://walter.marlinspike.hq.getpebble.com/ci/status/master'
@@ -20,26 +21,37 @@ OFF = (0, 0, 0)
 # You can create this environment variable to use a specific blink(1), otherwise it will use id 0
 BLINK1_ID = int(os.getenv('PEBBLE_MASTER_BUILD_STATUS_BLINK1_ID', 0))
 
-BLINK1CMD = sh.Command("blink1-tool")
+BLINK1CMD = sh.Command('blink1-tool')
 BLINK1CMD.bake('-d {}'.format(BLINK1_ID))
 
+
 def fade_to_color(fade_duration, r, g, b):
-  color_string = '0x{:02x},0x{:02x},0x{:02x}'.format(r, g, b)
-  duration_string = '{}'.format(fade_duration)
-  BLINK1CMD('--rgb', color_string, '--m', duration_string)
+    color_string = '0x{:02x},0x{:02x},0x{:02x}'.format(r, g, b)
+    duration_string = '{}'.format(fade_duration)
+    BLINK1CMD('--rgb', color_string, '-m', duration_string)
+
 
 def back_to_black():
-  fade_to_color(0, *OFF)
+    fade_to_color(0, *OFF)
+
+
+def main():
+    while True:
+        try:
+            try:
+                build_status_page = requests.get(WALTER_MASTER_STATUS_URL)
+                color = GREEN if (build_status_page.text == 'Successful') else RED
+            except requests.ConnectionError:
+                color = ORANGE
+            fade_to_color(FADE_DURATION_MS, *color)
+            sleep(STATUS_POLLING_INTERVAL_SECONDS)
+        except KeyboardInterrupt:
+            # let atexit handle it
+            sys.exit(0)
+
 
 if __name__ == '__main__':
-  # Turn the blink(1) off when script exits
-  atexit.register(back_to_black)
+    # Turn the blink(1) off when script exits
+    atexit.register(back_to_black)
 
-  while True:
-    try:
-      build_status_page = requests.get(WALTER_MASTER_STATUS_URL)
-      color = GREEN if (build_status_page.text == 'Successful') else RED
-    except requests.ConnectionError:
-      color = ORANGE
-    fade_to_color(FADE_DURATION_MS, *color)
-    sleep(STATUS_POLLING_INTERVAL_SECONDS)
+    main()
